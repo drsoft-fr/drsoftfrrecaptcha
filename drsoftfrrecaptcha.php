486e42b6b9328242b81091962798ff18342c7699
dylan <dylan.ramos.n@gmail.com>
2026-09-12 16:51:41 +0200

    Update repository URLs and package name to match renamed module

--- drsoftfrrecaptcha.php
+++ drsoftfrrecaptcha.php
@@ -11,7 +11,7 @@ use DrSoftFr\Module\ReCaptcha\Install\Factory\InstallerFactory;
 use DrSoftFr\Module\ReCaptcha\Install\Installer;
 use PrestaShop\PrestaShop\Core\Cache\Clearer\CacheClearerChain;
 
-if (!defined('_PS_VERSION_') || !defined('_CAN_LOAD_FILES_')) {
+if (!defined('_PS_VERSION_')) {
     exit;
 }
 
@@ -72,10 +72,10 @@ class drsoftfrrecaptcha extends Module
                 'wording_domain' => 'Modules.Drsoftfrrecaptcha.Admin',
             ],
         ];
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->authorEmail = 'contact@drsoft.fr';
-        $this->moduleGithubRepositoryUrl = 'https://github.com/drsoft-fr/prestashop-module-drsoftfrrecaptcha';
-        $this->moduleGithubIssuesUrl = 'https://github.com/drsoft-fr/prestashop-module-drsoftfrrecaptcha/issues';
+        $this->moduleGithubRepositoryUrl = 'https://github.com/drsoft-fr/drsoftfrrecaptcha';
+        $this->moduleGithubIssuesUrl = 'https://github.com/drsoft-fr/drsoftfrrecaptcha/issues';
         $this->isPsVersion8 = (bool)version_compare(_PS_VERSION_, '8.0', '>=');
 
         parent::__construct();
